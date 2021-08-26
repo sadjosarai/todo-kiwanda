b08573a27c778d70bacc50bc93cee25bec1c16aa
sadjosarai <sadjosarai@gmail.com>
2021-08-26 21:19:05 +0100

    header and footer configure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Icon from '@material-ui/core/Icon';
 
-function App() {
-  return (
+class Header extends React.Component{
+  render(){
+    return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>This the to DO app homework</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+    </div>
+  );
+  }
+}
+class Body extends React.Component{
+  render() {
+    return (
+      <div>
+        <p>this is the body</p>
+      </div>
+    );
+  }
+}
+class Footer extends React.Component{
+  render(){
+    return (
+      <div className="footer">
+        <div className="nav">
+        <div className="left">
+          <a className="App-link text-left"
+            href=" "
+            target="_blank"
+            rel="noopener noreferrer">
+            <Icon>arrow_left</Icon>
+            Previous Work
+          </a>
+        </div>
+        <div className="next">
+          <a className="App-link text-right"
+            href=" "
+            target="_blank"
+            rel="noopener noreferrer">
+            Next Work
+            <Icon>arrow_right</Icon>
+          </a>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
